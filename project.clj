b54b75d2a8c23afc6b1c99ee1cfd1745b9395927
Shulang Lei <shulang.lei@unbounce.com>
2018-02-02 13:48:50 -0800

    Pull in generative testing deps

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.unbounce/clojure-dogstatsd-client "0.1.1"
+(defproject com.unbounce/clojure-dogstatsd-client "0.1.2-SNAPSHOT"
   :description "A thin veneer over java-dogstatsd-client"
   :url "https://github.com/unbounce/clojure-dogstatsd-client"
-  :license {:name "The MIT License (MIT)"
-            :url "http://opensource.org/licenses/MIT"
+  :license {:name     "The MIT License (MIT)"
+            :url      "http://opensource.org/licenses/MIT"
             :comments "Copyright (c) 2018 Unbounce Marketing Solutions Inc."}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.datadoghq/java-dogstatsd-client "2.5"]]
   :global-vars {*warn-on-reflection* true})
